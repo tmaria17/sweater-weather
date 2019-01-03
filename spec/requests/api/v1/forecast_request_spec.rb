cec8938e687402aa1f679e820c7e39390ca8bf10
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-02 22:21:55 -0700

    Spec for forecast request.

---
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe "Forecast API" do
+  it 'should return weather info for user location' do
+    user_location = 'denver,co'
+
+    get "/api/v1/forecast?location=#{user_location}"
+
+    expect(response.status).to eq(200)
+
+    weather_data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(weather_data).to have_key(:data)
+    expect(weather_data[:data]).to have_key(:attributes)
+    expect(weather_data[:data][:attributes]).to have_key(:current_weather)
+    expect(weather_data[:data][:attributes]).to have_key(:hourly_temps)
+    expect(weather_data[:data][:attributes]).to have_key(:daily_weather)
+  end
+end
