82ddcadaf08655cce10a7755fe5fe4444354aa00
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-03 16:28:57 -0700

    account creation spec.

---
+++ spec/requests/api/v1/account_creation_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'account creation'do
+  it 'returns an api ket to user'do
+    user_info =
+    {
+      email: "whatever@example.com",
+      password: "password",
+      password_confirmation: "password"
+    }
+    user = User.new(user_info)
+    post '/api/v1/users/', params: user_info
+    user_results = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.status).to eq(200)
+
+
+  end
+end
