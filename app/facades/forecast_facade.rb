6f29de3fc2373c4ddb19ec439c157bb0d20eff6b
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-03 13:16:22 -0700

    Creates a forecast facade.

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,8 +1,36 @@
 class ForecastFacade
-
-  def coord_service
+  def initialize(location)
+    @location = location
+  end
+  def weather_info
+    weather_service.get_weather
+  end
+  def current_weather
+    weather_info[:currently]
   end
 
-  def weather_service
+  def daily_weather_summary
+    weather_info[:daily]
   end
+
+  def daily_weather
+    weather_info[:daily][:data]
+  end
+
+  def hourly_weather_summary
+    weather_info[:hourly]
+  end
+
+  def hourly_weather
+    weather_info[:hourly][:data]
+  end
+
+  private
+    def coord_service
+      CoordinateService.new(@location)
+    end
+
+    def weather_service
+      WeatherService.new(coord_service.get_coordinates)
+    end
 end
