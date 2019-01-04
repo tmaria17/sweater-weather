f77fd720839a7072a5c352e4adb3c342bc73ddf2
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-04 10:06:28 -0700

    Instances of forecast facade and gif service in controller.

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,4 +1,11 @@
 class Api::V1::GifsController < ApplicationController
   def index
+    forecast = ForecastFacade.new(params[:location])
+    gif_service = GifService.new(forecast.daily_weather[0].icon)
+    gifs = forecast.daily_weather.map do |data|
+      Gif.new(data.time, data.icon, gif_service.get_gif_data[:data][0][:url])
+    end
+
+    render json: gifs
   end
 end
