e91d2b7ff4a9c65ec0d3ddd0371e6802393718f7
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-03 23:00:31 -0700

    Changes serializer attributes.

--- app/serializers/forecast_facade_serializer.rb
+++ app/serializers/forecast_facade_serializer.rb
@@ -1,6 +1,5 @@
 class ForecastFacadeSerializer
   include FastJsonapi::ObjectSerializer
   set_id :location
-  attributes :current_weather, :daily_weather_summary, :daily_weather,
-             :hourly_weather_summary, :hourly_weather
+  attributes :current_weather, :daily_weather, :hourly_weather
 end
