75868b2111b532914d6e3cba226bba520719c41a
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-04 10:07:26 -0700

    Unskip request spec.

--- spec/requests/api/v1/giphy_request_spec.rb
+++ spec/requests/api/v1/giphy_request_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 describe 'GIPHY API' do
-  xit 'returns gifs based on location weather' do
+  it 'returns gifs based on location weather' do
     get '/api/v1/gifs?location=denver,co'
 
 
     gifs = JSON.parse(response.body, symobolize_names: true)
-
+    binding.pry
     expect(response.status).to eq(200)
     expect(gifs).to have_key(:data)
     expect(gifs[:data]).to have_key(:daily_forecasts)
