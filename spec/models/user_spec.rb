0686f05415f07d68e886712b006d8b7a766f0b50
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-03 17:00:50 -0700

    Test for user validations.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User do
+  describe 'validations' do
+    it {should validate_presence_of(:email)}
+    it {should validate_presence_of(:password)}
+  end
 end
