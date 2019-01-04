2e38a918b8c9ee516c19ac9bf5ce861cf444f50f
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-03 18:29:48 -0700

    Add more expections to test.

--- spec/requests/api/v1/account_creation_spec.rb
+++ spec/requests/api/v1/account_creation_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe 'account creation'do
-  it 'returns an api ket to user'do
+  it 'returns an api key to user'do
     user_info =
     {
-      email: "whatever@example.com",
+      email: "whateverforever@example.com",
       password: "password",
       password_confirmation: "password"
     }
@@ -12,6 +12,9 @@ describe 'account creation'do
     post '/api/v1/users/', params: user_info
     user_results = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response.status).to eq(200)
+    expect(response.status).to eq(201)
+    expect(user_results[:data]).to have_key(:attributes)
+    expect(user_results[:data][:attributes]).to have_key(:api_key)
+    expect(user_results[:data]).to have_key(:id)
   end
 end
