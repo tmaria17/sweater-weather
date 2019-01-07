956a1247bb483883245bf8621a11eaaeb1f1728d
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-07 11:41:03 -0700

    Creat login request spec and add vcr to giphy.

--- spec/requests/api/v1/giphy_request_spec.rb
+++ spec/requests/api/v1/giphy_request_spec.rb
@@ -2,16 +2,18 @@ require 'rails_helper'
 
 describe 'GIPHY API' do
   it 'returns gifs based on location weather' do
-    get '/api/v1/gifs?location=denver,co'
+    VCR.use_cassette("giphy_request") do
+      get '/api/v1/gifs?location=denver,co'
 
 
-    gifs = JSON.parse(response.body, symobolize_names: true)
-    expect(response.status).to eq(200)
-    expect(gifs).to have_key("data")
-    # expect(gifs[:data]).to have_key(:daily_forecasts)
-    # expect(gifs[:data]).to have_key(:copy_right)
-    # expect(gifs[:data][:daily_forecasts][0]).to have_key(:time)
-    # expect(gifs[:data][:daily_forecasts][0]).to have_key(:summary)
-    # expect(gifs[:data][:daily_forecasts][0]).to have_key(:url)
+      gifs = JSON.parse(response.body, symobolize_names: true)
+      expect(response.status).to eq(200)
+      expect(gifs).to have_key("data")
+      # expect(gifs[:data]).to have_key(:daily_forecasts)
+      # expect(gifs[:data]).to have_key(:copy_right)
+      # expect(gifs[:data][:daily_forecasts][0]).to have_key(:time)
+      # expect(gifs[:data][:daily_forecasts][0]).to have_key(:summary)
+      # expect(gifs[:data][:daily_forecasts][0]).to have_key(:url)
+    end
   end
 end
