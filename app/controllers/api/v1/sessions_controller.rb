916317f0f49252f843b80fa7ecab885de2e9f582
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-07 15:42:31 -0700

    Create method for sessions controller now authenticates.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    user = User.find_by_email(params[:email])
-    render json: UserSerializer.new(user)
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user)
+    end
   end
 end
