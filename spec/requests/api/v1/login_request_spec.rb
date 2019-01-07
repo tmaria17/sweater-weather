7821cebb029d709cbb5192f09c4d18fecf4030cb
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-07 14:11:33 -0700

    login request spec.

--- spec/requests/api/v1/login_request_spec.rb
+++ spec/requests/api/v1/login_request_spec.rb
@@ -2,16 +2,18 @@ require 'rails_helper'
 
 describe 'login' do
   it 'returns an api key when user logins' do
-    post '/api/v1/sessions'
+    user_info =
+    {
+      email: "whateverforever@example.com",
+      password: "password",
+      password_confirmation: "password"
+    }
+
+    user = User.new(user_info)
+    post '/api/v1/sessions', params: user_info
 
     data = JSON.parse(response.body, symobolize_names: true)
     expect(response.status).to eq(200)
-# Content-Type: application/json
-# Accept: application/json
-#
-# {
-#   "email": "whatever@example.com",
-#   "password": "password"
-# }
+    expect(data["data"]["attributes"]).to have_key("api_key")
   end
 end
