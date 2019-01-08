0aeeff9e5b67b9957f868df4a310c48995817663
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-07 21:10:24 -0700

    add request spec for favoriting a location.

---
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Favorite request post' do
+  it 'favorites a location' do
+    user = User.create(email: 'ABurr@president.com', password: 'Hamilton', password_confirmation: 'Hamilton')
+    favorite_params =  {
+                'location': 'New York, NY',
+                'api_key': user.api_key
+              }
+    post '/api/v1/favorites', params: favorite_params
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(data[:location]).to eq("New York, NY")
+    expect(data[:user_id]).to eq(user.id)
+  end
+end
