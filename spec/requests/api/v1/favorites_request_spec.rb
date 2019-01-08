6c8ae2b32cfe40ab343fa5e216810c65ccf48c32
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-07 22:09:56 -0700

    Finishes user story 4.

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -5,11 +5,11 @@ describe 'Favorite request post' do
     user = User.create(email: 'ABurr@president.com', password: 'Hamilton', password_confirmation: 'Hamilton')
     favorite_params =  {
                 'location': 'New York, NY',
-                'api_key': user.api_key
+                'api_key': "#{user.api_key}"
               }
-              
+
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    post '/api/v1/favorites', params: favorite_params
+    post '/api/v1/favorites/', params: favorite_params
     data = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
