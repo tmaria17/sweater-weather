bbfee6cd4259c2085ee6ef7f32bab829965e05cd
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-07 21:40:13 -0700

    renders json for favorites.

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::FavoritesController < ApplicationController
   def create
     if current_user
-      Favorite.create(user_id: current_user.id, location: params[:location])
+      favorite = Favorite.create(user_id: current_user.id, location: params[:location])
+      render json:favorite
     else
       render body: 'Unauthorized', status: 401
     end
