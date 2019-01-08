9159e69bc8ee01537b3933e633c6c8d9baa2938c
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-07 21:22:05 -0700

    Dream driven create method.

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::FavoritesController < ApplicationController
   def create
+    if current_user
+      Favorite.create(user_id: current_user.id, location: params[:location])
+    else
+      render body: 'Unauthorized', status: 401
+    end
   end
 end
