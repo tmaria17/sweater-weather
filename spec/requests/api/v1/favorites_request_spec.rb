62ff9a067a7f8318492c038e94cbb687b4ed73b5
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-08 19:47:17 -0700

    Sad path testing.

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -16,4 +16,18 @@ describe 'Favorite request post' do
     expect(result[:data][:attributes][:location]).to eq("New York, NY")
     expect(result[:data][:id]).to eq("1")
   end
+  it 'does not create an favorite if api key is nil' do
+    user = User.create(email: 'ABurr@president.com', password: 'Hamilton', password_confirmation: 'Hamilton')
+    favorite_params =  {
+                'location': 'New York, NY',
+                'api_key': nil
+              }
+
+    post '/api/v1/favorites/', params: favorite_params
+
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    expect(response.body).to eq("Unauthorized")
+  end
 end
--- spec/requests/api/v1/listing_favorite_locations_spec.rb
+++ spec/requests/api/v1/listing_favorite_locations_spec.rb
@@ -17,4 +17,18 @@ describe 'Get Favorites' do
     expect(results[:data][0][:attributes]).to have_key(:location)
     expect(results[:data][0][:attributes]).to have_key(:current_weather)
   end
+  it "does not list locations if user doesn't have a correct api key" do
+    user = User.create(email: 'ABurr@president.com', password: 'Hamilton', password_confirmation: 'Hamilton')
+    favorite_1 = user.favorites.create(location: 'Denver, CO')
+    favorite_2 = user.favorites.create(location: 'Allen, TX')
+    favorite_params =  {
+                'api_key': "abc"
+              }
+
+    get '/api/v1/favorites/', params: favorite_params
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    expect(response.body).to eq("Unauthorized")
+  end
 end
