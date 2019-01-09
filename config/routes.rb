ec1b5edb66cee4057bdd70a43050ecb8f8971a6d
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-08 17:49:06 -0700

    add route for favorites index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'forecast', to: 'forecasts#show'
-      get 'gifs', to: 'gifs#index'
+      get  'forecast', to: 'forecasts#show'
+      get  'gifs', to: 'gifs#index'
       post '/users', to: 'users#create'
       post '/sessions', to: 'sessions#create'
       post '/favorites', to: 'favorites#create'
-
+      get  '/favorites', to: 'favorites#index'
     end
   end
 end
