6e391f878123548f8fa45be118da6f2f8e5f306e
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-08 22:13:58 -0700

    Adds sad path test for sessions create.

--- spec/requests/api/v1/login_request_spec.rb
+++ spec/requests/api/v1/login_request_spec.rb
@@ -11,11 +11,31 @@ describe 'login' do
     user = User.create!(user_info)
 
     post '/api/v1/sessions/', params: user_info
-    
+
     data = JSON.parse(response.body, symbolize_names: true)
 
     expect(response.status).to eq(200)
     expect(data[:data][:attributes]).to have_key(:api_key)
     expect(data[:data][:attributes]).to_not eq(nil)
+  end
+  it 'does not let you login with wrong info' do
+    user_info =
+    {
+      email: "helloworld1@example.com",
+      password: "password",
+      password_confirmation: "password"
+        }
+    wrong_info =
+    {
+      email: "helloworld1@example.com",
+      password: "car",
+      password_confirmation: "car"
+        }
+    user = User.create!(user_info)
+
+    post '/api/v1/sessions/', params: wrong_info
+
+    expect(response.status).to eq(404)
+    expect(response.body).to eq("Not  Found")
   end
 end
