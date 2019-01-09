7ff1d82aa19d8c4a8a14f6fdf3e80edf958a2079
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-08 22:04:46 -0700

    minor changes.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
-    if  user && user.authenticate(params[:password])
+    if user.authenticate(params[:password])
       session[:user_id] = user.id
       render json: UserSerializer.new(user)
+    else
+      render json: "Not  Found", status: 404
     end
   end
 end
