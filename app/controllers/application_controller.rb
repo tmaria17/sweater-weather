d15d50867968dfb7b5e48c42e9a83998621262ba
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-08 19:29:20 -0700

    Add helper method to authorize user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   helper_method :current_user
+
   def current_user
    @current_user ||= User.find(session[:user_id]) if session[:user_id]
- end
+  end
+
+  def authorize_user
+    user = User.find_by(api_key: params[:api_key])
+    unless user
+       render json: "Unauthorized", status: 401
+    end
+  end
+
 end
