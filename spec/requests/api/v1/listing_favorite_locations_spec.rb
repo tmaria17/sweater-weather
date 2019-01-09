432a5f0a96b1c77ea6f1ab28eea7926136fceeec
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-01-09 11:01:37 -0700

    Adds favorite vcr.

--- spec/requests/api/v1/listing_favorite_locations_spec.rb
+++ spec/requests/api/v1/listing_favorite_locations_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 describe 'Get Favorites' do
   it 'lists favorite locations' do
-    user = User.create(email: 'ABurr@president.com', password: 'Hamilton', password_confirmation: 'Hamilton')
-    favorite_1 = user.favorites.create(location: 'Denver, CO')
-    favorite_2 = user.favorites.create(location: 'Allen, TX')
-    favorite_params =  {
+    VCR.use_cassette("favorites") do
+      user = User.create(email: 'ABurr@president.com', password: 'Hamilton', password_confirmation: 'Hamilton')
+      favorite_1 = user.favorites.create(location: 'Denver, CO')
+      favorite_2 = user.favorites.create(location: 'Allen, TX')
+      favorite_params =  {
                 'api_key': "#{user.api_key}"
               }
+      get '/api/v1/favorites/', params: favorite_params
+      results = JSON.parse(response.body, symbolize_names: true)
 
-    get '/api/v1/favorites/', params: favorite_params
-    results = JSON.parse(response.body, symbolize_names: true)
-
-    expect(response).to be_successful
-    expect(results).to have_key(:data)
-    expect(results[:data][0]).to have_key(:attributes)
-    expect(results[:data][0][:attributes]).to have_key(:location)
-    expect(results[:data][0][:attributes]).to have_key(:current_weather)
+      expect(response).to be_successful
+      expect(results).to have_key(:data)
+      expect(results[:data][0]).to have_key(:attributes)
+      expect(results[:data][0][:attributes]).to have_key(:location)
+      expect(results[:data][0][:attributes]).to have_key(:current_weather)
+    end
   end
   it "does not list locations if user doesn't have a correct api key" do
     user = User.create(email: 'ABurr@president.com', password: 'Hamilton', password_confirmation: 'Hamilton')
